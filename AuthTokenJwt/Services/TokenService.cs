8cc16545263ce6ddfc37db665753528ea9a32526
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2022-08-16 17:00:05 -0300

    first commit

---
+++ AuthTokenJwt/Services/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using AuthTokenJwt.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthTokenJwt.Services
+{
+    public static class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler(); //gera um token
+            var key = Encoding.ASCII.GetBytes(Settings.Secret); //recebe a chave privada da aplicação
+            var tokenDescriptor = new SecurityTokenDescriptor //info descritas dentro do token, no caso as clains
+            {
+                Subject = new ClaimsIdentity(new Claim[] //descrição do token
+                {
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ AuthTokenJwt/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthTokenJwt
+{
+    public static class Settings //chave privada
+    {
+        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+    }
+}
